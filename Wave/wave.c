780c1cf88d69a92da842621e5e720814b8c19871
Lucas <jouvetlucas@yahoo.fr>
2018-03-24 18:59:16 +0100

    modifié :         wave.c

--- Wave/wave.c
+++ Wave/wave.c
@@ -19,6 +19,8 @@ void tracerCourbe(int clicx, int clicy, double dx, int dec_x, int dec_y, double 
 void tracerSegment(double dx, int dec_x, int dec_y, double zoom, double x1, double y1, double x2, double y2, int l, int h,int bool);
 void tracerPoint(double dx, int dec_x, double zoom, double x1, double y1, int l, int h);
 
+void detectionnotes(double ff);
+
 
 
 void usage(char* s){   /* explique le fonctionnement du programme */
@@ -48,7 +50,7 @@ int main(int argc, char** argv){
     int filtre = 1, precision = 1, l = 1200, h = 750, op_son = 0, anim = 0, debug = 0, verbeux = 0;   // les options
     int op;    /* sert a determiner les options selectionner */
 
-    int ampmax = 0,*tfre,ntfre = 0;
+    int ampmax = 0,*tfre,ntfre = 0,ampmaxold = 0;
     double frequence = 0, frequencemax = -1000000,t1 = 0,t2 = 0,t1max,t2max;
     
     short* amplitudes;
@@ -224,6 +226,9 @@ int main(int argc, char** argv){
 	     if(t2-t1!=0){
 	       frequence = 1/(t2-t1);
 	     }else frequence = 1/t2;
+	     if(ampmax > 2*ampmaxold){
+	       frequencemax = 0;
+	     }
 	     if(frequence > frequencemax){
 	       t1max = t1;
 	       t2max = t2;
@@ -233,6 +238,7 @@ int main(int argc, char** argv){
 	   ntfre ++;
 	   tfre = realloc(tfre,sizeof(int)*ntfre);
 	   tfre[ntfre-1] = compteur/filtre;
+	   ampmaxold = ampmax;
 	   ampmax = amp;
 	   t1 = t2;
 	     
@@ -259,7 +265,8 @@ int main(int argc, char** argv){
          // on avance de filtre-1 elements dans le fichier (accelere le traitement)
          fseek(fich, (long) ((filtre-1)*(bitsPerSample/8)), SEEK_CUR);
     }
-    
+    printf("%f\n",frequencemax);
+    detectionnotes(frequencemax);
     //MLV_Keyboard_button touche;
     if (son != NULL && op_son)
         MLV_play_sound(son, 1.0f);
@@ -477,5 +484,48 @@ void recupere_mot(char mot[5], FILE* fich, int debug){    // recupere un mot de 
 
 }*/
 
+void detectionnotes(double ff){
+  double notes[12][8]={{32.7 ,  65.41, 130.81, 261.63, 523.25, 1046.50, 2093.00, 4186.01},
+		  {34.65,  69.30, 138.59, 277.18, 554.37, 1108.73, 2217.46, 4434.92},
+		  {36.71,  73.42, 146.83, 293.66, 587.33, 1174.66, 2349.32, 4698.64},
+		  {38.89,  77.78, 155.56, 311.13, 622.25, 1244.51, 2489.02, 4978.03},
+		  {41.20,  82.41, 164.81, 329.63, 659.26, 1318.51, 2637.02, 5274.04},
+		  {43.65,  87.31, 174.61, 349.23, 698.46, 1396.91, 2793.83, 5587.65},
+		  {46.25,  92.50, 185.00, 369.99, 739.99, 1479.98, 2959.96, 5919.91},
+		  {49.00,  98.00, 196.00, 392.00, 783.99, 1567.98, 3135.96, 6271.93},
+		  {51.91, 103.83, 207.65, 415.30, 830.61, 1661.22, 3322.44, 6644.88},
+		  {55.00, 110.00, 220.00, 440.00, 880.00, 1760.00, 3520.00, 7040.00},
+		  {58.27, 116.54, 233.08, 466.16, 932.33, 1864.66, 3729.31, 7458.62},
+		  {61.74, 123.47, 246.94, 493.88, 987.77, 1975.53, 3951.07, 7902.13}};
+  char *notesS[]={"do","do","re","re","mi","mi","fa","fa","sol","sol","la","la","si"};
+
+  int i,j,fin;
+  fin = 0;
+  for(j=0;j<8;j++){
+    for(i=0;i<12;i++){
+      if(ff < notes[i][j]+1){
+	if(j == 0){
+	  printf("note : %s\n",notesS[11]);
+	  fin = 1;
+	  break;
+	}
+	else{
+	  printf("note : %s\n",notesS[i-1]);
+	  fin = 1;
+	  break;
+	}
+	if(i == 11 && j == 7){
+	  printf("note : %s\n",notesS[11]);
+	  fin= 1;
+	  break;
+	}
+      }
+    }
+    if(fin == 1){
+      break;
+    }
+  }
+  
+}
 
 
