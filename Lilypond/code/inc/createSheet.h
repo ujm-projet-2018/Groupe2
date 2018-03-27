1353e28252d340ddd6959bda14280738eb43b285
Tao <tao.grolleau@openmailbox.org>
2018-03-27 13:28:22 +0200

    Version correcte et complete de l'itération 1

---
+++ Lilypond/code/inc/createSheet.h
@@ -0,0 +1,25 @@
+#ifndef CREATE_SHEET_H
+#define CREATE_SHEET_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "math.h"
+
+/*
+Fonction : void init_ly(FILE* f);
+Permet d'initialiser les premiere lignes d'une partition Lilypond
+FILE* f : fichier d'écriture de la partition
+*/
+void init_ly(FILE* f);
+
+/*
+Fonction : void lire_ecrire(FILE* input, FILE* output);
+Ecris le fichier "input" en langage lilypond dans output
+FILE* output : fichier d'écriture de la partition
+*/
+void lire_ecrire(FILE* input, FILE* output);
+
+void decoupage_avant_barre (double temps, int metrique, char note, FILE* output);
+void decoupage_apres_barre (double temps, int metrique, char note, FILE* output);
+
+#endif
