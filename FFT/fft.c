71dece53987da55d0d61f57ede3fad9a1176dd47
Bhastyen <bastien.duraj@live.fr>
2018-04-03 15:18:41 +0200

    Finalisation du module de test de la FFT. Modification des options du programme.

--- FFT/fft.c
+++ FFT/fft.c
@@ -7,17 +7,20 @@
 
 
 
-#define PI 3.14156
+#define PI 3.141592645
 #define ZOOM_X 1000.0
 #define VITESSE_DX 10
 #define VITESSE_ZOOM 1.05
 
 
 void recupere_mot(char mot[5], FILE* fich, int debug);
-complex* analyse(short* amplitudes, int depart, int N);
-complex* recupere_point_complex(short* amplitudes, int depart, int n);
+complex* analyse(short* amplitudes, int depart, int fin, int N);
+complex* recupere_point_complex(short* amplitudes, int depart, int fin, int n);
 void tracer_spectre(complex* fft, int clicx, int clicy, int n, double dx, double zoomx, int dec_x, int dec_y, double echelley, int freqEch, int l, int h);
-void tracer_repere_spectre(int clicx, int clicy, double dx, double zoomx, int dec_x, int dec_y, double max, int n, double T, int l, int h);
+void tracer_repere_spectre(int clicx, int clicy, double dx, double zoomx, int dec_x, int dec_y, double max, int n, int freqEch, int l, int h);
+void affiche_tab(short* amplitudes, int echan_fourier);
+short* signal_sinus(int n);
+
 
 
 void usage(char* s){   /* explique le fonctionnement du programme */
@@ -31,8 +34,9 @@ void usage(char* s){   /* explique le fonctionnement du programme */
   fprintf(stderr,"      -d : active les informations utiles pour le debugage du programme\n");
     
   fprintf(stderr,"   Definir la taille des parametres suivant:\n");
-  fprintf(stderr,"      -n \"entier\" > 1 : pour definir la fenetre de l'analyse de fourier(nombre de point)\n");
-  fprintf(stderr,"      -0 \"entier\" > 1 : pour definir le point de depart de l'analyse de fourier\n");
+  fprintf(stderr,"      -0 \"entier\" > 1 : pour definir le temps auquel commence l'analyse de fourier\n");
+  fprintf(stderr,"      -t \"entier\" > 1 : pour definir la duree de la fenetre utilisee pour l'analyse de fourier\n");
+  fprintf(stderr,"      -n \"entier\" > 1 : pour definir le nombre de point utilise dans la fenetre de l'analyse de fourier (doit etre une puissance de 2)\n");
   fprintf(stderr,"      -p \"entier\" > 1 : pour definir la precision de la collecte des données\n");
   fprintf(stderr,"      -l \"entier\" > 1 : pour definir la largeur de la fenetre\n");
   fprintf(stderr,"      -h \"entier\" > 1 : pour definir la hauteur de la fenetre\n");
@@ -41,8 +45,8 @@ void usage(char* s){   /* explique le fonctionnement du programme */
 
 int main(int argc, char** argv){
     int compteur = 0, arret = 0, nb_point = 0, dec_x, dec_y, clicx = 0, clicy = 0, echelley = 700;   
-    int freqEch, echantillon, defausse_entier, bytePerSec, taille, longueur;   // donnees du fichier WAVE
-    int l = 1200, h = 750, echan_fourier = 0, filtre = 1, debug = 1, depart = 0;   // les options
+    int freqEch = 44100, echantillon, defausse_entier, bytePerSec, taille, longueur;   // donnees du fichier WAVE
+    int l = 1200, h = 750, nb_point_fourier = 1024, filtre = 1, debug = 1, depart = 0, longueur_fenetre = 1, fin = 0;   // les options
     int op;    /* sert a determiner les options selectionner */
 
     complex* fft;
@@ -63,7 +67,7 @@ int main(int argc, char** argv){
     }
 
     /********** traitement des options entrees sur la ligne de commande ********/
-    while ((op = getopt(argc, argv, "n:0:f:l:h:p:ud")) != -1){   /* cherche les options sur la ligne de commande */
+    while ((op = getopt(argc, argv, "t:n:0:f:l:h:p:ud")) != -1){   /* cherche les options sur la ligne de commande */
       switch (op){    /* determine l'option recuperer */
       case 'd':
         debug = 1;
@@ -71,14 +75,17 @@ int main(int argc, char** argv){
       case 'u':   /* affiche l'usage du programme */
         usage(argv[0]);
         exit(0);
-      case 'n':    /* permet de preciser la valeur de l'echantillonage */
-        echan_fourier = atoi(optarg);  /* recupere la valeur situer juste apres l'option */
+      case 'n':    /* permet de preciser le nombre de point utilise */
+        nb_point_fourier = atoi(optarg);  /* recupere la valeur situer juste apres l'option */
         break;
       case 'f':     /* precise le fichier a utiliser */
         nomFich = optarg;
         break;
-      case '0':     /* point de depart de l'analyse */
+      case '0':     /* temps de depart en ms de l'analyse */
         depart = atoi(optarg);
+        break;
+      case 't':     /* duree en ms de l'analyse */
+        longueur_fenetre = atoi(optarg);
         break;
       case 'l':  /* precise la largeur de la fenetre */
         l = atoi(optarg);
@@ -96,7 +103,7 @@ int main(int argc, char** argv){
     }
     
     // verifie si le fichier a bien ete donne et la fenetre une taille valide
-    if (nomFich == NULL || l < 800 || h < 600){
+    if (l < 800 || h < 600){
         usage(argv[0]);
         exit(-1);
     }
@@ -106,8 +113,8 @@ int main(int argc, char** argv){
     
     fich = fopen(nomFich, "rb");
     if (fich == NULL){
-         fprintf(stderr, "Probleme lors de l'ouverture du fichier\n");
-         exit(-1);
+        fprintf(stderr, "Probleme lors de l'ouverture du fichier\n");
+        exit(-1);
     }
     
     // creation de la fenetre MLV
@@ -119,6 +126,7 @@ int main(int argc, char** argv){
         if (mot[0] == 'R' && mot[1] == 'I' && mot[2] == 'F' && mot[3] == 'F'){   // on traite les blocs importants: RIFF, cue, fmt
             fread(&taille, sizeof(int), 1, fich);  //FileSize
             fread(&defausse_entier, sizeof(int), 1, fich);  //FileFormatID
+            // TODO si autre que 'WAVE' alors on quitte car le fichier n'est pas un fichier wave
         }else if (mot[0] == 'c' && mot[1] == 'u' && mot[2] == 'e'){
             fread(&longueur, sizeof(int), 1, fich);  // CUE CHUNK longueur du bloc
             fseek(fich, (long) longueur, SEEK_CUR);    // se deplace de la longuer du bloc
@@ -139,7 +147,6 @@ int main(int argc, char** argv){
             fseek(fich, (long) longueur, SEEK_CUR);    // se deplace de la longueur du bloc
         }
         recupere_mot(mot, fich, debug);   // recupere le mot DataBlocID
-        sleep(1);
     }
     fread(&echantillon, sizeof(int), 1, fich);  //DataSize util pour arreter la boucle principale
     
@@ -147,9 +154,6 @@ int main(int argc, char** argv){
     filtre *= nbCanaux;
     // calcule du nombre de point a recupere dans les tableaux
     nb_point = (echantillon/((bitsPerSample/8)*filtre));
-    // defini le nombre de point pour la FFT
-    if (echan_fourier == 0)
-         echan_fourier = nb_point;
 
     // afichage des donnees du fichier WAVE
     fprintf(stderr, "\nSpecification du fichier:\n");
@@ -182,10 +186,16 @@ int main(int argc, char** argv){
          // on avance de filtre-1 elements dans le fichier (accelere le traitement)
          fseek(fich, (long) ((filtre-1)*(bitsPerSample/8)), SEEK_CUR);
     }
+
+    // calcul des indices depart et fin
+    fin = (((depart+longueur_fenetre)/1000.0)*freqEch);
+    depart = ((depart/1000.0)*freqEch);
+    // lancement de l'analyse
+    fft = analyse(amplitudes, depart, fin, log2(nb_point_fourier));
     
-    fft = analyse(amplitudes, depart, log2(echan_fourier));
+    fprintf(stderr, "depart = %d(%f), fin = %d(%f), freq = %lf\n", depart, temps[depart], fin, temps[fin], (longueur_fenetre/1000.0)/nb_point_fourier);
     
-    tracer_spectre(fft, clicx, clicy, pow(2, log2(echan_fourier)), dx, zoom, dec_x, dec_y, echelley, freqEch, l, h);
+    tracer_spectre(fft, clicx, clicy, /*pow(2, log2(*/nb_point_fourier/*))*/, dx, zoom, dec_x, dec_y, echelley, /*(longueur_fenetre/1000.0)/nb_point_fourier*/nb_point_fourier, l, h);
     // visualisation du graphe
     while (!arret){
          // nettoyage de la fenetre
@@ -204,31 +214,31 @@ int main(int argc, char** argv){
              // nettoyage de la fenetre
              MLV_clear_window(MLV_rgba(0, 0, 0, 255));
              // appel la fonction de tracer
-             tracer_spectre(fft, clicx, clicy, pow(2, log2(echan_fourier)), dx, zoom, dec_x, dec_y, echelley, freqEch, l, h);
+             tracer_spectre(fft, clicx, clicy, /*pow(2, log2(*/nb_point_fourier/*))*/, dx, zoom, dec_x, dec_y, echelley, /*(longueur_fenetre/1000.0)/nb_point_fourier*/nb_point_fourier, l, h);
          }else if (MLV_get_keyboard_state(275) == MLV_PRESSED){   // fleche gauche
              dx -= VITESSE_DX*(1.0/zoom);
              // nettoyage de la fenetre   
              MLV_clear_window(MLV_rgba(0, 0, 0, 255));
              // appel la fonction de tracer
-             tracer_spectre(fft, clicx, clicy, pow(2, log2(echan_fourier)), dx, zoom, dec_x, dec_y, echelley, freqEch, l, h);
+             tracer_spectre(fft, clicx, clicy, /*pow(2, log2(*/nb_point_fourier/*))*/, dx, zoom, dec_x, dec_y, echelley, /*(longueur_fenetre/1000.0)/nb_point_fourier*/nb_point_fourier, l, h);
          }else if (MLV_get_keyboard_state(274) == MLV_PRESSED){     // fleche bas
              zoom /= VITESSE_ZOOM;
              // nettoyage de la fenetre   
              MLV_clear_window(MLV_rgba(0, 0, 0, 255));
              // appel la fonction de tracer
-             tracer_spectre(fft, clicx, clicy, pow(2, log2(echan_fourier)), dx, zoom, dec_x, dec_y, echelley, freqEch, l, h);
+             tracer_spectre(fft, clicx, clicy, /*pow(2, log2(*/nb_point_fourier/*))*/, dx, zoom, dec_x, dec_y, echelley, /*(longueur_fenetre/1000.0)/nb_point_fourier*/nb_point_fourier, l, h);
          }else if (MLV_get_keyboard_state(273) == MLV_PRESSED){      // fleche haut
              zoom *= VITESSE_ZOOM;
              // nettoyage de la fenetre   
              MLV_clear_window(MLV_rgba(0, 0, 0, 255));
              // appel la fonction de tracer
-             tracer_spectre(fft, clicx, clicy, pow(2, log2(echan_fourier)), dx, zoom, dec_x, dec_y, echelley, freqEch, l, h);
+             tracer_spectre(fft, clicx, clicy, /*pow(2, log2(*/nb_point_fourier/*))*/, dx, zoom, dec_x, dec_y, echelley, /*(longueur_fenetre/1000.0)/nb_point_fourier*/nb_point_fourier, l, h);
          }else if (MLV_get_mouse_button_state(MLV_BUTTON_LEFT) == MLV_PRESSED){
              MLV_get_mouse_position(&clicx, &clicy);
              // nettoyage de la fenetre   
              MLV_clear_window(MLV_rgba(0, 0, 0, 255));
              // appel la fonction de tracer
-             tracer_spectre(fft, clicx, clicy, pow(2, log2(echan_fourier)), dx, zoom, dec_x, dec_y, echelley, freqEch, l, h);
+             tracer_spectre(fft, clicx, clicy, /*pow(2, log2(*/nb_point_fourier/*))*/, dx, zoom, dec_x, dec_y, echelley, /*(longueur_fenetre/1000.0)/nb_point_fourier*/nb_point_fourier, l, h);
          }else if (MLV_get_keyboard_state(27) == MLV_PRESSED){
              arret = 1;
          }
@@ -260,8 +270,8 @@ double tab_max(complex* tab, int n){
     return max;
 }
 
-complex* recupere_point_complex(short* amplitudes, int depart, int n){
-     int i;
+complex* recupere_point_complex(short* amplitudes, int depart, int fin, int n){
+     int i; int pas;
      complex* tab = (complex*) malloc(sizeof(complex)*n);
      if (tab == NULL){
          fprintf(stderr, "fft.c::recupere_point_complexe()::probleme d'allocation memoire\n");
@@ -277,8 +287,14 @@ complex* recupere_point_complex(short* amplitudes, int depart, int n){
           return tab;
      }
      
-     for (i=0; i<n; i++){
-         tab[i] = amplitudes[depart+i]+I*0;
+     pas = (fin-depart)/n;
+     if (pas == 0){
+         fprintf(stderr, "Pas assez de point dans l'intervalle pour faire l'analyse de fourier\n");
+         exit(-1);
+     }
+     
+     for (i=0; i<(n*pas); i+=pas){
+         tab[i/pas] = amplitudes[depart+i]+I*0;
      }
      
      
@@ -339,13 +355,13 @@ void calculFFT(complex* freq, complex* y, int q, int e){
 }
 
 
-complex* analyse(short* amplitudes, int depart, int q){
+complex* analyse(short* amplitudes, int depart, int fin, int q){
      int e, N = pow(2, q);
      complex* echange;
      complex* freq;
-     complex* y = recupere_point_complex(NULL, 0, N);
+     complex* y = recupere_point_complex(NULL, 0, 0, N);
      
-     freq = recupere_point_complex(amplitudes, depart, N);
+     freq = recupere_point_complex(amplitudes, depart, fin, N);
      // rempli 'y' a partir de 'x' en redistribuant les valeurs de 'x' au indice inverse bit a bit de 'y'
      distributionInversionBits(q, freq, y);
      
@@ -379,7 +395,7 @@ void tracerSegment(double dx, int dec_x, int dec_y, double zoom, double x1, doub
     
     // tracer du segment grace a MLV
     if (x1>=0 && x2>=0 && y1>=0 && y2>=0 && x1<=l && x2<=l && y1<=h && y2<=h)
-        MLV_draw_line(x1, (h-dec_y)-y1, x2, (h-dec_y)-y2, MLV_rgba(255,0,0,255));
+        MLV_draw_line((int) x1, (int) (h-dec_y)-y1, (int) x2, (int) (h-dec_y)-y2, MLV_rgba(255,0,0,255));
     
     /*glBegin(GL_LINES);
     //glColor3f(255*(1.0/255.0), 0, 0);
@@ -396,18 +412,18 @@ void tracer_spectre(complex* fft, int clicx, int clicy, int n, double dx, double
      double x = 0, y = cabs(fft[0])*echelley/max;
      
      for (i = 0; i<n-1; i++){
-          tracerSegment(dx, l/2, dec_y, zoomx, x, y, i+1, cabs(fft[i+1])*echelley/max, l, h);
-          x = (i+1);
+          tracerSegment(dx, l/2, dec_y, zoomx, x, y, ((double) (i+1)/n)*freqEch, cabs(fft[i+1])*echelley/max, l, h);
+          x = ((double) (i+1)/n)*freqEch;
           y = cabs(fft[i+1])*echelley/max;
      }
      
-     tracer_repere_spectre(clicx, clicy, dx, zoomx, dec_x, dec_y, 1.0, n, 1.0/freqEch, l, h);
+     tracer_repere_spectre(clicx, clicy, dx, zoomx, dec_x, dec_y, 1.0, n, freqEch, l, h);
      
      MLV_actualise_window();
 }
 
 
-void tracer_repere_spectre(int clicx, int clicy, double dx, double zoomx, int dec_x, int dec_y, double max, int n, double T, int l, int h){
+void tracer_repere_spectre(int clicx, int clicy, double dx, double zoomx, int dec_x, int dec_y, double max, int n, int freqEch, int l, int h){
     int i, pas = 100, nb_grad = 10;
     double valeur, czoom;
     int tailleText = 0;
@@ -429,6 +445,7 @@ void tracer_repere_spectre(int clicx, int clicy, double dx, double zoomx, int de
         valeur = (clicx-dx)-czoom;   // je recentre ma valeur x-dx sur l'origine
         valeur /= zoomx;    // je fais la mise a l'echelle inverse au zoom
         valeur += czoom;   // je repositionne ma valeur
+        valeur *= ((double) freqEch)/n;
         sprintf(valeurs,"(%.3lf, %.3lf)", valeur, (double) (h-dec_y-clicy)*(max/h));
     }
     MLV_draw_text(l-150, 25, valeurs, MLV_rgba(0, 255, 0, 255));
@@ -441,6 +458,7 @@ void tracer_repere_spectre(int clicx, int clicy, double dx, double zoomx, int de
             valeur = i-dx-czoom;   // je recentre ma valeur i-dx sur l'origine
             valeur /= zoomx;    // je fais la mise a l'echelle inverse au zoom
             valeur += czoom;   // je repositionne ma valeur
+            valeur *= ((double) freqEch)/n;
             sprintf(text,"%.3lf", valeur);
             MLV_get_size_of_text(text, &tailleText, NULL);
             MLV_draw_line(i, h-5, i, h-15, MLV_rgba(0, 0, 255, 255));
@@ -475,4 +493,32 @@ void recupere_mot(char mot[5], FILE* fich, int debug){    // recupere un mot de 
     fread(mot+2, sizeof(char), 1, fich);
     fread(mot+3, sizeof(char), 1, fich);
 }
+
+
+short* signal_sinus(int n){
+    int i;
+    short* amp = (short*) malloc(sizeof(short)*n);
+    if (amp == NULL){
+        fprintf(stderr, "fft.c::signal_sinus()::probleme allocation memoire\n");
+        exit(-1);
+    }
+
+    for (i = 0; i<n; i++)
+        amp[i] = sin(2.0*PI*i/n) + 0.5*sin(4.0*PI*i/n) + 0.25*cos(10.0*PI*i/n)*32767.0;
+
+    return amp;
+}
+
+
+void affiche_tab(short* amplitudes, int echan_fourier){
+    int i;
+    
+    for (i=0; i<echan_fourier; i++)
+         fprintf(stdout, "%d %d\n", i, amplitudes[i]);
+    fprintf(stdout, "fin\n");
+    
+}
+
+
+
 
