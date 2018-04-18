f728f26d8254f18fb8593bcf9b697f9ef4b6d073
Bhastyen <bastien.duraj@live.fr>
2018-04-18 22:54:10 +0200

    Seconde validation avant merge.

--- Wave/detection_notes.c
+++ Wave/detection_notes.c
@@ -1,11 +1,11 @@
 #include "detection_notes.h"
-
+#include <string.h>
 int inf(elem e1,elem e2){
   return e1<e2;
 }
   
 
-void detectionnotes(double ff){
+char* detectionnotes(double ff){
   double notes[12][8]={{32.7 ,  65.41, 130.81, 261.63, 523.25, 1046.50, 2093.00, 4186.01},
 		       {34.65,  69.30, 138.59, 277.18, 554.37, 1108.73, 2217.46, 4434.92},
 		       {36.71,  73.42, 146.83, 293.00, 587.33, 1174.66, 2349.32, 4698.64},
@@ -19,14 +19,15 @@ void detectionnotes(double ff){
 		       {58.27, 116.54, 233.08, 466.16, 932.33, 1864.66, 3729.31, 7458.62},
 		       {61.74, 123.47, 246.94, 493.88, 987.77, 1975.53, 3951.07, 7902.13}};
   char *notesS[]=      {"do","do#","re","re#","mi","fa","fa#","sol","sol#","la","la#","si"};
-
   int i,j,fin;
+  char *note = (char*) malloc(sizeof(char)*20);
   fin = 0;
   for(j=0;j<8;j++){
     for(i=0;i<12;i++){
       if(i == 11){
 	if(ff < (notes[i][j]+(notes[0][j+1] - notes[i][j])/2) && ff > (notes[i][j]-(notes[i][j]-notes[i-1][j])/2) ) {
-	    printf("note : %s\n",notesS[i]);
+	  sprintf(note,"%s %d",notesS[i],j);
+	  printf("note : %s %d\n",notesS[i],j);
 	    fin = 1;
 	    break;
 	}
@@ -34,14 +35,16 @@ void detectionnotes(double ff){
       else if(i == 0){
 	if(ff < (notes[i][j]+(notes[i+1][j] - notes[i][j])/2) && ff > (notes[i][j]-(notes[i][j]-notes[11][j-1])/2) ) {
 	  if(i == 0){
-	    printf("note : %s\n",notesS[0]);
+	    sprintf(note,"%s %d",notesS[0],j);
+	    printf("note : %s %d\n",notesS[0],j);
 	    fin = 1;
 	    break;
 	  }
 	}
       }
       else if(ff < (notes[i][j]+(notes[i+1][j] - notes[i][j])/2) && ff > (notes[i][j]-(notes[i][j]-notes[i-1][j])/2) ) {
-	printf("note : %s\n",notesS[i]);
+	sprintf(note,"%s %d",notesS[i],j);
+	printf("note : %s %d\n",notesS[i],j);
 	fin = 1;
 	break;
       }
@@ -50,7 +53,7 @@ void detectionnotes(double ff){
       break;
     }
   }
-  
+  return note;
 }
 
 
@@ -122,4 +125,67 @@ double cleanFrequence(double *tabfreq,int nfre){
   frequencemax /= nfre;
 
   return frequencemax;
+}
+
+signal analyse_notes(short *amp, float *temps, int nb_point){
+  int i;
+  short am;
+
+  signal s;
+  s.ntfre = 0;
+  s.nfre = 0;
+  
+  int ampmax = 0, /**tfre, ntfre = 0,nfre=0,*/ ampmaxold = 0;
+  double frequence = 0, frequencemax = -1000000, t1 = 0/*,*tabfreq*/;
+  float t2 = 0.0;
+  s.tfre = (int*) malloc(sizeof(int)*1);
+  s.tabfreq = (double*) malloc(sizeof(double)*1);
+  
+  for(i=0;i<nb_point;i++){
+    am = amp[i];
+    /* am = amp[i]/500;
+    if(am > 1){
+      am*=10000;
+      }*/
+    t2 = temps[i];
+    if (ampmax == 0){
+      ampmax = am;
+    }
+    if(am > ampmax-(ampmax*0.30) && am < ampmax*1.30 && t2 > t1+0.001){
+      if(ampmax != 0){
+	frequence = 1/(t2-t1); 
+	if(frequence > frequencemax){  // enregistre la nouvelle frequence max
+	  // recupere les indices de la frequence maximale
+	  frequencemax = frequence;
+	}
+      }
+      s.ntfre ++;
+      s.nfre ++;
+      s.tfre = realloc(s.tfre,sizeof(int)*s.ntfre);
+      s.tabfreq = realloc(s.tabfreq,sizeof(double)*s.nfre);
+      s.tabfreq[s.nfre-1] = frequence;
+      s.tfre[s.ntfre-1] = nb_point;
+      ampmaxold = ampmax;
+      ampmax = am;
+      t1 = t2;
+      
+    }else if (am > ampmax ){
+      ampmax = am;
+    }
+  }
+
+  frequencemax = cleanFrequence(s.tabfreq,s.nfre);
+  printf("Frequence 1 : %f\n",frequencemax);
+  detectionnotes(frequencemax);
+  frequencemax = cleanFrequence2(s.tabfreq,s.nfre);
+  printf("\nFrequence 2 : %f\n",frequencemax);
+  detectionnotes(frequencemax);
+  frequencemax = cleanFrequence3(s.tabfreq,s.nfre);
+  printf("\nFrequence 3 : %f\n",frequencemax);
+  detectionnotes(frequencemax);
+  frequencemax = cleanFrequence4(s.tabfreq,s.nfre);
+  printf("\nFrequence 4 : %f\n",frequencemax);
+  s.note = detectionnotes(frequencemax);
+  return s;
+  
 }
