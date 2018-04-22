0db31172af1d3e3c335f2350eccd8a63af4c2a32
fkempenaers <francis.kempenaers@gmail.com>
2018-04-22 19:29:09 +0200

    Gestion durees silences accords, ajouts des midi tests 	modified:   ParserMidi/ParserMidi.java 	new file:   ParserMidi/mid/AuClairDeLaLune.mid 	new file:   ParserMidi/mid/AuClairDeLaLune34.mid 	new file:   ParserMidi/mid/FurElise.mid 	new file:   ParserMidi/mid/accords.mid 	new file:   ParserMidi/mid/dieses.mid 	new file:   ParserMidi/mid/durees.mid 	new file:   ParserMidi/mid/durees2.mid 	new file:   ParserMidi/mid/durees3.mid 	new file:   ParserMidi/mid/silences.mid 	new file:   ParserMidi/mid/test.mid

--- ParserMidi/ParserMidi.java
+++ ParserMidi/ParserMidi.java
@@ -1,4 +1,3 @@
-package projtut;
 
 import java.io.File;
 import java.nio.charset.Charset;
@@ -16,121 +15,144 @@ import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Track;
 
 public class ParserMidi {
-	public static final int NOTE_ON = 0x90;
-	public static final int NOTE_OFF = 0x80;
-	public static final String[] NOTE_NAMES = { "c 0", "c 1", "d 0", "d 1", "e 0", "f 0", "f 1", "g 0", "g 1", "a 0",
-			"a 1", "b 0" };
-	public static long notesTab[] = new long[129];
+    public static final int NOTE_ON = 0x90;
+    public static final int NOTE_OFF = 0x80;
+    public static final String[] NOTE_NAMES = { "c 0", "c 1", "d 0", "d 1", "e 0", "f 0", "f 1", "g 0", "g 1", "a 0",
+						"a 1", "b 0" };
+    public static long notesTab[] = new long[129];
 
-	public static void main(String[] args) throws Exception {
-		Sequence sequence = MidiSystem.getSequence(new File(args[0]));
-		for (int i = 0; i < 129; i++)
-			notesTab[i] = 0;
+    public static void main(String[] args) throws Exception {
+	Sequence sequence = MidiSystem.getSequence(new File(args[0]));
+	for (int i = 0; i < 129; i++)
+	    notesTab[i] = 0;
 
-		Charset utf8 = StandardCharsets.UTF_8;
-		Path path = Paths.get("notes.txt");
+	Charset utf8 = StandardCharsets.UTF_8;
+	Path path = Paths.get("notes.txt");
 
-		String str = "";
-		int trackNumber = 0, duree;
-		long deltaTick, ecart;
-		long tick = 0, lastTick = 0;
-		int accord = 0;
-		deltaTick=ecart=0;
+	String str = "";
+	int trackNumber = 0, duree,silence=0;
+	long deltaTick, ecart;
+	long tick = 0, lastTick = 0,nextTick = 0,deb = 0, fin = 0;
+	int accord = 0,lastAccord=0;
+	deltaTick=ecart=0;
 
-		for (Track track : sequence.getTracks()) {
-			trackNumber++;
-			System.out.println("Track " + trackNumber + ": size = " + track.size());
-			System.out.println();
+	for (Track track : sequence.getTracks()) {
+	    trackNumber++;
+	    System.out.println("Track " + trackNumber + ": size = " + track.size());
+	    System.out.println();
 
-			int nbNotes = 0;
+	    int nbNotes = 0;
 
-			for (int j = 0; j < track.size(); j++) {
-				MidiEvent event = track.get(j);
-				MidiMessage message = event.getMessage();
-				if (message instanceof ShortMessage) {
-					ShortMessage sm = (ShortMessage) message;
-					if (sm.getCommand() == NOTE_ON && sm.getData2() > 0)
-						nbNotes++;
-				}
-			}
-			System.out.println(nbNotes);
-			str = Integer.toString(nbNotes) + "\n" + "4 4" + "\n";
-			Files.write(path, str.getBytes(utf8), StandardOpenOption.CREATE);
-
-			double resolution = sequence.getResolution();
-			double ticksPerSecond = resolution * (120 / 60.0);
-			double tickSize = 1.0 / ticksPerSecond;
-			System.out.println("ticksize :" + tickSize);
-			System.out.println("resolution :" + sequence.getResolution());
-
-			for (int i = 0; i < track.size(); i++) {
-
-				MidiEvent event = track.get(i);
-
-				lastTick = tick;
-				tick = event.getTick();
-
-				System.out.print("@" + event.getTick() + " ");
-				MidiMessage message = event.getMessage();
-
-				if (message instanceof ShortMessage) {
-					ShortMessage sm = (ShortMessage) message;
-					System.out.print("Channel: " + sm.getChannel() + " ");
-
-					if (sm.getCommand() == NOTE_ON) {
-						int key = sm.getData1();
-						int octave = (key / 12) - 1;
-						int note = key % 12;
-						String noteName = NOTE_NAMES[note];
-						int velocity = sm.getData2();
-						deltaTick = 0;
-
-						// calcul de la duree
-						if (velocity != 0) {
-							notesTab[key] = tick;
-
-						} else {
-							deltaTick = tick - notesTab[key];
-							notesTab[key] = 0;
-							
-							ecart = tick - lastTick;
-							if (ecart != 0) {
-
-								accord = (accord+1)%2;
-							}
-						}
-
-						// note octave dièse temps silence durée
-
-						if (deltaTick != 0) {
-							duree =  (int) (2 / (deltaTick * tickSize));
-							str += noteName + " " + octave + " " + duree + " 0 " + accord+" ";
-							str += "\n";
-						}
-
-						System.out.println("Note on, " + noteName + octave + " key=" + key + " velocity: " + velocity);
-					} else {
-						System.out.println("Command:" + sm.getCommand() + " " + sm.getData1() + " " + sm.getData2());
-					}
-				} else {
-					StringBuilder tempo = new StringBuilder();
-					byte[] tempobytes = message.getMessage();
-
-					/*
-					 * for(int k = 3; k < message.getLength();k++) {
-					 * tempo.append(String.format("%02X", tempobytes[k])); }
-					 */
-
-					for (byte b : message.getMessage()) {
-						tempo.append(String.format("%02X ", b));
-					}
-					System.out.println("Other message: " + message.getClass() + " " + message.getStatus() + " "
-							+ tempo.toString() + " ");
-				}
-				Files.write(path, str.getBytes(utf8), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
-			}
-			System.out.println();
+	    for (int j = 0; j < track.size(); j++) {
+		MidiEvent event = track.get(j);
+		MidiMessage message = event.getMessage();
+		if (message instanceof ShortMessage) {
+		    ShortMessage sm = (ShortMessage) message;
+		    if (sm.getCommand() == NOTE_ON && sm.getData2() > 0)
+			nbNotes++;
 		}
+	    }
+	    System.out.println(nbNotes);
+	    str = Integer.toString(nbNotes) + "\n" + "4 4" + "\n";
+	    Files.write(path, str.getBytes(utf8), StandardOpenOption.CREATE);
 
+	    double resolution = sequence.getResolution();
+	    double ticksPerSecond = resolution * (120 / 60.0);
+	    double tickSize = 1.0 / ticksPerSecond;
+	    System.out.println("ticksize :" + tickSize);
+	    System.out.println("resolution :" + sequence.getResolution());
+
+	    for (int i = 0; i < track.size(); i++) {
+
+		MidiEvent event = track.get(i);
+
+		System.out.print("@" + event.getTick() + " ");
+		MidiMessage message = event.getMessage();
+
+		if (message instanceof ShortMessage) {
+		    ShortMessage sm = (ShortMessage) message;
+		    System.out.print("Channel: " + sm.getChannel() + " ");
+
+		    if (sm.getCommand() == NOTE_ON) {
+						
+			lastTick = tick;
+			tick = event.getTick();
+			nextTick = track.get(i+1).getTick();
+						
+			int key = sm.getData1();
+			int octave = (key / 12) - 1;
+			int note = key % 12;
+			String noteName = NOTE_NAMES[note];
+			int velocity = sm.getData2();
+			deltaTick = 0;
+
+
+			// calcul de la duree
+			if (velocity != 0) {
+			    notesTab[key] = tick;
+			    deb = tick;
+
+			} else {
+			    deltaTick = tick - notesTab[key];
+			    notesTab[key] = 0;
+							
+			    if(nextTick-tick != 0) {
+							
+				silence = (int) (2 / ((nextTick - tick) * tickSize))*2;
+				silence = (int) (Math.log(silence)/Math.log(2));
+				silence = (int) Math.pow(2, silence);
+				silence = silence > 32 ? 0 : silence;
+			    }else {
+				silence = 0;
+			    }
+							
+			    ecart = nextTick - tick;
+			    if (ecart == 0) {
+				if(lastAccord!=1) {
+				    accord = 1;
+				}else {
+				    accord = 2;
+				}
+			    }else if(tick - lastTick ==0){
+				lastAccord = accord;
+			    }else {
+				accord = lastAccord = 0;
+			    }
+			}
+
+			// note octave dièse duree silence(=temps avant prochaine note) accord
+
+			if (deltaTick != 0) {
+			    duree =  (int) (2 / (deltaTick * tickSize));
+			    duree = (int) (Math.log(duree)/Math.log(2));
+			    duree = (int) Math.pow(2, duree);
+			    str += noteName + " " + octave + " " + duree + " "+silence+" " + accord+" ";
+			    str += "\n";
+			}
+
+			System.out.println("Note on, " + noteName + octave + " key=" + key + " velocity: " + velocity);
+		    } else {
+			System.out.println("Command:" + sm.getCommand() + " " + sm.getData1() + " " + sm.getData2());
+		    }
+		} else {
+		    StringBuilder tempo = new StringBuilder();
+		    byte[] tempobytes = message.getMessage();
+
+		    /*
+		     * for(int k = 3; k < message.getLength();k++) {
+		     * tempo.append(String.format("%02X", tempobytes[k])); }
+		     */
+
+		    for (byte b : message.getMessage()) {
+			tempo.append(String.format("%02X ", b));
+		    }
+		    System.out.println("Other message: " + message.getClass() + " " + message.getStatus() + " "
+				       + tempo.toString() + " ");
+		}
+		Files.write(path, str.getBytes(utf8), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+	    }
+	    System.out.println();
 	}
+
+    }
 }
